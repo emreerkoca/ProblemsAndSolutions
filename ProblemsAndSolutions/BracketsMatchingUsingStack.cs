9924e36144c4a705c26b50c8892efb92acb752a2
Emre Erkoca <emreerkoca@outlook.com>
2018-10-10 01:12:38 +0300

    Brackets matches problem added
    Brackets matches problem added. Stack was used.

---
+++ ProblemsAndSolutions/BracketsMatchingUsingStack.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemsAndSolutions
+{
+    class BracketsMatchingUsingStack
+    {
+        //Problem: Brackets matching
+        //Right formats: ({}), (()){}(), ({}())
+        //Wrong formats: ({}(, ){}), (()
+        //Write a method with a string parameter
+        //if string patemeter value is right formats return true
+        //else return false
+
+        public bool GetBracketsMatchingBool(string parenthesis)
+        {
+            bool control = false;
+            Stack<Char> stack = new Stack<Char>(); 
+
+            for(int i = 0; i < parenthesis.Length; i++)
+            {
+                char c = parenthesis[i];
+                if (c == '{' || c == '(')
+                {
+                    stack.Push(c);
+                }
+                else if(c == '}' || c == ')')
+                {
+                    if(stack.Count() == 0)
+                    {
+                        control = false;
+                        return control;
+                    }
+
+                    if(c == '}')
+                    {
+                        if (stack.Peek() == '{')
+                        {
+                            stack.Pop();
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    else if(c == ')')
+                    {
+                        if (stack.Peek() == '(')
+                        {
+                            stack.Pop();
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+
+            return stack.Count() == 0;
+        }
+
+    }
+}
--- ProblemsAndSolutions/Program.cs
+++ ProblemsAndSolutions/Program.cs
@@ -9,49 +9,12 @@ namespace ProblemsAndSolutions
 {
     class Program
     {
-        class Worker
-        {
-            private volatile int counter = 0;
-
-            public void DoWork(object data)
-            {
-                while(true)
-                {
-                    var apple = new Apple();
-                    counter++;
-                    Console.WriteLine("Created apple count: {0}", data + "-" + counter.ToString());
-                }
-            }
-
-         
-        }
-
         static void Main(string[] args)
         {
-            //Problem:
-            //5 threads running at the same time. 
-            //Each one creates a new variable apple. 
-            //How many apple are generated?
+            //MultipleThreadSample multipleThreadSample = new MultipleThreadSample();
 
-            Worker workerObj = new Worker();
-            Thread threadA = new Thread(workerObj.DoWork);
-            Thread threadB = new Thread(workerObj.DoWork);
-            Thread threadC = new Thread(workerObj.DoWork);
-            Thread threadD = new Thread(workerObj.DoWork);
-            Thread threadE = new Thread(workerObj.DoWork);
-
-
-            threadA.Start("A");
-            threadB.Start("B");
-            threadC.Start("C");
-            threadD.Start("D");
-            threadE.Start("E");
-
-        }
-
-        class Apple
-        {
-
+            BracketsMatchingUsingStack bracketsMatching = new BracketsMatchingUsingStack();
+            bool control = bracketsMatching.GetBracketsMatchingBool("{()}");
         }
     }
 }
